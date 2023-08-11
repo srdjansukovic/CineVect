1ad644beba1ee877ef5e2c059dd216e2766cf6bb
srdjansukovic <srdjansukovic@gmail.com>
2023-08-11 16:32:16 +0200

    Dockerized cinevect application

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from query import create_embedding_from_query, query_search
 
 app = Flask(__name__)
 
@@ -6,6 +7,26 @@ app = Flask(__name__)
 def hello():
     return "Hello, Flask!"
 
-@app.route('/about')
-def about():
-    return "This is the about page."
+@app.route('/query', methods=['POST'])
+def post_example():
+    try:
+        request_body = request.get_json()
+
+        query = request_body.get('query')
+        filter = request_body.get('filter')
+        n_results = request_body.get('n_results')
+
+        if not query or not filter or not n_results:
+            return jsonify({"error": "Sentence, filter and n_results are required fields."}), 400
+
+        embedding = create_embedding_from_query(query=query)
+        pinecone_filter = parse_filter(filter)
+        query_response = query_search(embedding=embedding, filter=pinecone_filter, n_results=n_results)
+
+        return query_response.to_dict()
+
+    except Exception as e:
+        return jsonify({"error": "An error occurred.", "details": str(e)}), 500
+    
+def parse_filter(user_filter):
+    return {key: {"$eq": value} for key, value in user_filter.items()}
--- query.py
+++ query.py
@@ -1,22 +1,31 @@
 from configuration import model, index
 
-sentences = [
-    {
-        "id": "1",
-        "text": " The FBI raids and shuts down Stratton Oakmont. Despite this one breach, Jordan receives a reduced sentence for his testimony and serves three years in a minimum security prison.",
-        "metadata": {"genre": "Unknown", "year": 1901}
-    }
-]
+# sentences = [
+#     {
+#         "id": "1",
+#         "text": " The FBI raids and shuts down Stratton Oakmont. Despite this one breach, Jordan receives a reduced sentence for his testimony and serves three years in a minimum security prison.",
+#         "metadata": {"genre": "Unknown", "year": 1901}
+#     }
+# ]
 
-embeddings = model.encode([sentence["text"] for sentence in sentences]).tolist()
+# embeddings = model.encode([sentence["text"] for sentence in sentences]).tolist()
 
-query_response = index.query(
-    vector=embeddings[0],
-    filter={
-        "genre": {"$eq": "Comedy"}
-    },
-    top_k=3,
-    include_metadata=True
-)
+# query_response = index.query(
+#     vector=embeddings[0],
+#     filter={
+#         "genre": {"$eq": "Comedy"}
+#     },
+#     top_k=3,
+#     include_metadata=True
+# )
 
-print(query_response)
+def create_embedding_from_query(query):
+    return model.encode(query).tolist()
+
+def query_search(embedding, filter, n_results):
+    return index.query(
+        vector=embedding,
+        filter=filter,
+        top_k=n_results,
+        include_metadata=True
+    )
